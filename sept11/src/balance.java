cd79e291e50c4c20b89654616da41d6d68d82cc7
mnavarr3 <navarromelyssa@gmail.com>
2019-09-11 15:41:08 -0500

    2nd version

--- sept11/src/balance.java
+++ sept11/src/balance.java
@@ -1,3 +1,21 @@
-public class balance {
+import java.util.Scanner;
 
+public class balance {
+    public static void main (String[] args) {
+        int balance = 100;
+        String text = "";
+        while (balance > 0) {
+            text = "Your balance: \u0024" + balance + ".";
+            text += "\nAmount to withdraw:";
+
+            System.out.println(text);
+
+            Scanner input = new Scanner(System.in);
+            int amount = input.nextInt();
+
+            balance = balance - amount;
+        }
+        System.out.println("Balance is " + balance + ". Nothing left.");
+        text += "\nIt is now " + balance + " \u0024" + " ";
+    }
 }

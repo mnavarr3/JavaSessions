7edb6d4faff47b369994011aea78b9611794bb07
mnavarr3 <navarromelyssa@gmail.com>
2019-11-06 15:24:17 -0600

    1st version

---
+++ nov6/src/Example3/Store.java
@@ -0,0 +1,11 @@
+package Example3;
+
+public class Store {
+    public static void main(String[] args){
+
+        //creating an instance of type Order using empty default constructor
+        WarehouseOrder myfirstOrder = new WarehouseOrder();
+
+        myfirstOrder.process();
+    }
+}
---
+++ nov6/src/Example4/NonAnnoyingCustomer.java
@@ -0,0 +1,10 @@
+package Example4;
+
+public class NonAnnoyingCustomer implements AllThingsConsidered, AlmostForgotAboutThat {
+    public void sayHello(){
+        System.out.println("I will always be nice.");
+    }
+    public void SayBye(){
+        System.out.println("I'm really grateful for your service.");
+    }
+}

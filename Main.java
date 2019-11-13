d4b508196c32218ce5fff44d4ede1c0bf5d5f259
mnavarr3 <54876961+mnavarr3@users.noreply.github.com>
2019-11-13 06:18:39 -0600

    Add files via upload
    finished?

--- Main.java
+++ Main.java
@@ -6,43 +6,68 @@ import javafx.event.EventHandler;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.layout.HBox;
 import javafx.stage.Stage;
 
+import java.util.ArrayList;
+
 public class Main extends Application {
+    //let's create an array list variable to hold our items
+    ArrayList<Item> items = new ArrayList<Item>();
 
     @Override
     public void start(Stage primaryStage) throws Exception{
         //Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
 
 
-        //here is you rplace to put code
+        //here is your place to put code
         //let's create some UI elements
         Button addButton = new Button();
         addButton.setText("Add Item");
 
+        Button openBox = new Button("Show description");
+
         //create a label
         Label mylabel = new Label("hello");
 
-        //another way of writing it (works exactly the same!!!)
+        //make addButton to do something when clicked
         addButton.setOnAction(new EventHandler<ActionEvent>() {
             @Override
             public void handle(ActionEvent actionEvent) {
                 //here we put our code - actions or things that should happen when the button has been pressed
-                Item myitem = new Item();
-                myitem.setValue(678.0);
+                //use the constructor for class item with 3 variables
+                Item myitem = new Item(456789, 678.0, "this is a beloved item");
+
+                //now lets add this item to our array list (this becomes handy if we need to access our items at another place
+                items.add(myitem);
+                // create another Item object use the constructor for class item with one String variable (or in other words "the String constructor"
+                Item mySecondItem = new Item("this is a very special item");
+                //let's add this one also to our items array list
+                items.add(mySecondItem);
                 //print value of item on the console
                 System.out.println(myitem.getValue());
+
+                // ... or using the ArrayList object
+                System.out.println(items.get(0).getValue());
 
             }
         });
 
-
+        //another way of writing code to make button to do some action (works exactly the same!!!)
+        openBox.setOnAction(e-> {
+            //create dialog box which shows the description of the first item
+            Alert alertBox = new Alert(Alert.AlertType.INFORMATION);
+            //add the description using out items array list object
+            alertBox.setContentText(items.get(0).getDescription());
+            // show the dialog
+            alertBox.show();
+        });
 
         HBox HBox1 = new HBox(10);
-        HBox1.getChildren().addAll(addButton, mylabel);
+        HBox1.getChildren().addAll(addButton, openBox, mylabel);
 
         primaryStage.setTitle("Hello World");
         primaryStage.setScene(new Scene(HBox1, 300, 275));
@@ -55,60 +80,9 @@ public class Main extends Application {
     }
 }
 
-class Item {
 
 
-    //field variables
-    private long propertyID;
-    private double value;
-
-    //getters and setters
-    public double getValue() {
-        return value;
-    }
-
-    public void setValue(double value) {
-        this.value = value;
-    }
-
-    public long getPropertyID() {
-        return propertyID;
-    }
-
-    public void setPropertyID(long propertyID) {
-        this.propertyID = propertyID;
-    }
-
-    //method to calculate value loss
-    public double looseValue() {
-        double result = 0.0;
-        result = this.getValue() * 0.8;
-        return result;
-    }
 
 
-}
 
-class Server extends Item {
 
-}
-
-class Rack extends Item {
-
-    public double looseValue() {
-        double result = 0.0;
-        result = this.getValue() * 0;
-        return result;
-    }
-
-}
-
-class Desktop extends Item {
-
-    //overriding the loosValue method
-    public double looseValue() {
-        double result = 0.0;
-        result = this.getValue() * 0.6;
-        return result;
-    }
-}
